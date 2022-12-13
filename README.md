1db8e576cecaed195a2b2bd3b6e6e013f71ba3ab
LI LIU <lliu95877@gmail.com>
2022-12-12 22:53:05 -0500

    Add starter files

---
+++ README.md
@@ -0,0 +1,3 @@
+# dbt project for Adgile
+
+## Purpose: transform data in the Snowflake warehouse into useful datasets for BI tools and analytics
---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,11 @@
+select 
+    id as payment_id,
+    orderid as order_id, 
+    paymentmethod as payment_method,
+    status,
+    AMOUNT / 100 as amount,
+    created as created_at,
+    _BATCHED_AT
+    
+from {{ source('stripe', 'payment') }}
+
